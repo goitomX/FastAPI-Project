26c0f3cd5a6a8fd14ee499316f82d62aea060ed1
drax <goitomkebede1010@gmail.com>
2025-07-01 09:41:15 +0300

    add app/branches.py - alist of braches as enum

---
+++ app/braches.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Braches(str, Enum):
+    DISTRICT1 = "District1"
+    DISTRICT2 = "District2"
+    DISTRICT3 = "Arbaminch"
+    DISTRICT4 = "Sodo"
+    DISTRICT5 = "Hossana"
+    DISTRICT6 = "Karate"
